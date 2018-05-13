fbfca082b8afb023bdcdf81e20568d4b8285e8c6
Desali <sultansailaubaev1998@gmail.com>
2018-05-13 14:12:23 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { FindComponent } from './find/find.component';
+import { HomeComponent } from './home/home.component';
+
+const routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'find',
+    component: FindComponent
+  }
+]
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FindComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
